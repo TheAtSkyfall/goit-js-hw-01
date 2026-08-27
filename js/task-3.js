f307f11500f7e6921a93a1c9e70a2ff9ea15b3f9
KozachenkoVitalij <varvarprod2@gmail.com>
2026-08-27 13:19:19 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function getElementWidth(content, padding, border) {
-  return Number(content) + Number(padding) * 2 + Number(border) * 2;
+  return content + padding * 2 + border * 2;
 }
-console.log(getElementWidth("50", "8", "4")); // 74
-console.log(getElementWidth("60", "12", "8.5")); // 101
-console.log(getElementWidth("200", "0", "0")); // 200
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
